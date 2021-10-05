c32bc949cf03c48382c92bd6918dfe82e232fbdf
GitHub Action <action@github.com>
2021-10-05 13:02:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pranaymohapatra/autovaluetodataclass/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pranaymohapatra.autovaluetodataclass.services
+
+import com.github.pranaymohapatra.autovaluetodataclass.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pranaymohapatra/autovaluetodataclass/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pranaymohapatra/autovaluetodataclass/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pranaymohapatra.autovaluetodataclass.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pranaymohapatra.autovaluetodataclass.MyBundle
 
 class MyProjectService(project: Project) {
 
